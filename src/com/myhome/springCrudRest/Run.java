adc7861ad45c5d654dfaefa2bcc2897973ddb883
Nikolay Dolgopolov <nickolanick26@gmail.com>
2019-03-09 20:05:09 +0300

    add Spring

---
+++ src/com/myhome/springCrudRest/Run.java
@@ -0,0 +1,10 @@
+package com.myhome.springCrudRest;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class  Run {
+    public static void main(String[] args) {
+            ApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+    }
+}
