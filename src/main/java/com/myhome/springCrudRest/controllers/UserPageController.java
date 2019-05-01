6a5b84854473eaa8e101b0d621a3528927f07e68
Nikolay Dolgopolov <nickolanick26@gmail.com>
2019-05-01 17:57:07 +0300

    Spring boot + thymeleaf + bootstrap

--- src/main/java/com/myhome/springCrudRest/controllers/UserPageController.java
+++ src/main/java/com/myhome/springCrudRest/controllers/UserPageController.java
@@ -1,13 +1,9 @@
 package com.myhome.springCrudRest.controllers;
 
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Nick Dolgopolov (nick_kerch@mail.ru; https://github.com/Absent83/)
